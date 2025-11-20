4e2bd9aa206519e408a3e58e065fc7e855f7a603
syoch <syoch64@gmail.com>
2025-11-20 21:34:04 +0900

    Update: Some

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/25.05";
+  inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
     {
       nixpkgs,
+      rust-overlay,
       ...
     }:
     let
       system = "x86_64-linux";
 
       lib = pkgs.callPackage ./lib { };
+      overlays = [ (import rust-overlay) ];
 
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { inherit system overlays; };
       rpkgs = import ./pkgs/default.nix {
         pkgs = pkgs;
       };
--- shell.nix
+++ shell.nix
@@ -3,7 +3,10 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     clang-tools
 
-    cargo
+    (rust-bin.stable.latest.default.override {
+      extensions = [ "rust-src" ];
+      targets = [ "thumbv7em-none-eabi" ];
+    })
     rust-analyzer
     pkg-config
     udev
@@ -19,13 +22,22 @@ pkgs.mkShell {
 
     nix-output-monitor
 
+    graphviz
+    nix-tree
+
     #* Migrated from robotics container
     # Tools
     gcc-arm-embedded-14
     cmake
     go-task
+    rust-cbindgen
     rpkgs.qemu-arm-xpack
+
+    pkgs.llvmPackages.libclang.lib
   ];
+
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
   RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
 
   CMAKE_MODULE_PATH =
@@ -35,6 +47,7 @@ pkgs.mkShell {
         rpkgs.gcc-arm-toolchain
         rpkgs.static-mbed-os-f303k8
         rpkgs.static-mbed-os-f446re
+        rpkgs.club-legacy-libs
       ];
     in
     pkgs.lib.concatStringsSep ";" (map (p: "${p}/lib/cmake") roboPkgs);
