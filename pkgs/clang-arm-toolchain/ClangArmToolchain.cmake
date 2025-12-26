b70ead93c93f1c3580bd5878b3a2e607c02da39a
syoch <syoch64@gmail.com>
2025-12-26 09:44:09 +0900

    Update: Make standalone ClangArmToolchain

--- pkgs/clang-arm-toolchain/ClangArmToolchain.cmake
+++ pkgs/clang-arm-toolchain/ClangArmToolchain.cmake
@@ -1,23 +1,26 @@
-# ==============================================================================
-# Clang ARM Toolchain File for arm-none-eabi
-# ==============================================================================
-
-# ------------------------------------------------------------------------------
-# System Configuration
-# ------------------------------------------------------------------------------
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR ARM)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
+set(CMAKE_C_COMPILER_TARGET arm-none-eabi)
 set(CMAKE_C_COMPILER "@ClangRootDir@/bin/clang")
+set(CMAKE_C_FLAGS_INIT "-fshort-enums")
+
+set(CMAKE_CXX_COMPILER_TARGET arm-none-eabi)
 set(CMAKE_CXX_COMPILER "@ClangRootDir@/bin/clang++")
-set(CMAKE_ASM_COMPILER "@ClangRootDir@/bin/clang")
-set(CMAKE_AR "@LLVMRootDir@/bin/llvm-ar" CACHE FILEPATH "Archiver")
-set(CMAKE_RANLIB "@LLVMRootDir@/bin/llvm-ranlib" CACHE FILEPATH "Ranlib")
-set(CMAKE_OBJCOPY "@ArmToolchainDir@/bin/arm-none-eabi-objcopy" CACHE FILEPATH "Objcopy")
-set(CMAKE_OBJDUMP "@ArmToolchainDir@/bin/arm-none-eabi-objdump" CACHE FILEPATH "Objdump")
-set(CMAKE_SIZE "@ArmToolchainDir@/bin/arm-none-eabi-size" CACHE FILEPATH "Size")
+set(CMAKE_CXX_FLAGS_INIT "-fshort-enums")
+
+set(CMAKE_ASM_COMPILER "@ArmToolchainDir@/bin/arm-none-eabi-gcc")
+
+set(CMAKE_OBJCOPY "@ArmToolchainDir@/bin/arm-none-eabi-objcopy")
+set(CMAKE_OBJDUMP "@ArmToolchainDir@/bin/arm-none-eabi-objdump")
+set(CMAKE_SIZE "@ArmToolchainDir@/bin/arm-none-eabi-size")
+set_property(GLOBAL PROPERTY ELF2BIN "@ArmToolchainDir@/bin/arm-none-eabi-objcopy")
+
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
 
 # ------------------------------------------------------------------------------
 # Compiler Flags
@@ -32,46 +35,7 @@ include_directories(SYSTEM
     "@ArmToolchainDir@/lib/gcc/arm-none-eabi/14.2.1/include"
 )
 
-set(CMAKE_C_FLAGS_INIT "-target arm-none-eabi -ffunction-sections -fdata-sections")
-set(CMAKE_CXX_FLAGS_INIT "-target arm-none-eabi -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti")
-set(CMAKE_ASM_FLAGS_INIT "-target arm-none-eabi")
+set(CMAKE_CXX_LINK_EXECUTABLE "@ArmToolchainDir@/bin/arm-none-eabi-g++ <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_C_LINK_EXECUTABLE "@ArmToolchainDir@/bin/arm-none-eabi-gcc <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 
-# ------------------------------------------------------------------------------
-# Linker Flags
-# ------------------------------------------------------------------------------
-set(ARM_LINKER_FLAGS
-    "-target arm-none-eabi"
-    "-Wl,--gc-sections"
-    "-Wl,--no-dynamic-linker"
-    "-L@ArmToolchainDir@/lib/gcc/arm-none-eabi/14.2.1/thumb/v7e-m+fp/softfp"
-    "-L@ArmToolchainDir@/lib/gcc/arm-none-eabi/14.2.1"
-    "-L@ArmToolchainDir@/arm-none-eabi/lib/thumb/v7e-m+fp/softfp"
-    "-L@ArmToolchainDir@/arm-none-eabi/lib"
-    "-Wl,--defsym=_start=Reset_Handler"
-    "-Wl,--allow-multiple-definition"
-    "-nostdlib++"
-    "-fno-exceptions"
-    "-fno-rtti"
-    "-rtlib=libgcc"
-    "-Wl,--start-group"
-    "-lstdc++_nano"
-    "-lsupc++"
-    "-lm"
-    "-lc_nano"
-    "-lgcc"
-    "-lnosys"
-    "-Wl,--end-group"
-    "--ld-path=@ArmLinker@"
-)
-
-string(REPLACE ";" " " ARM_LINKER_FLAGS_STR "${ARM_LINKER_FLAGS}")
-
-set(CMAKE_EXE_LINKER_FLAGS "${ARM_LINKER_FLAGS_STR}")
-
-# ------------------------------------------------------------------------------
-# Cleanup: Unset Internal Variables
-# ------------------------------------------------------------------------------
-# Unset temporary variables used in this file
-unset(ARM_LINKER_FLAGS)
-unset(ARM_LINKER_FLAGS_STR)
-unset(ARM_TARGET_TRIPLE)
+set(USING_TOOLCHAIN "LLVM")
--- pkgs/clang-arm-toolchain/default.nix
+++ pkgs/clang-arm-toolchain/default.nix
@@ -3,29 +3,37 @@
   llvmPackages_19,
   gcc-arm-embedded,
 }:
-stdenv.mkDerivation {
-  pname = "clang-arm-toolchain";
-  version = "0.2.0";
+let
+  drv = stdenv.mkDerivation {
+    pname = "clang-arm-toolchain";
+    version = "0.2.0";
 
-  src = ./.;
+    src = ./.;
 
-  buildPhase = ''
-    echo "Building Clang Arm Toolchain"
-    cat $src/ClangArmToolchain.cmake \
-      | sed \
-          -e 's|@ArmToolchainDir@|${gcc-arm-embedded}|g' \
-          -e 's|@ClangRootDir@|${llvmPackages_19.clang-unwrapped}|g' \
-          -e 's|@LLVMRootDir@|${llvmPackages_19.llvm}|g' \
-          -e 's|@ArmLinker@|${llvmPackages_19.bintools}/bin/ld.lld|g' \
-      > ClangArmToolchain.cmake \
-      || echo "Error processing ClangArmToolchain.cmake"
-  '';
+    cmakeBuildInputs = [ ]; # Mark as CMakePackage
 
-  installPhase = ''
-    mkdir -p $out/lib/cmake
-    cp ClangArmToolchain.cmake $out/lib/cmake/ClangArmToolchain.cmake
-  '';
-}
-// {
-  cmakeBuildInputs = [ ];
-}
+    buildPhase = ''
+      echo "Building Clang Arm Toolchain"
+      cat $src/ClangArmToolchain.cmake \
+        | sed \
+            -e 's|@ArmToolchainDir@|${gcc-arm-embedded}|g' \
+            -e 's|@ClangRootDir@|${llvmPackages_19.clang-unwrapped}|g' \
+        > ClangArmToolchain.cmake \
+        || echo "Error processing ClangArmToolchain.cmake"
+    '';
+
+    installPhase = ''
+      mkdir -p $out/lib/cmake
+      cp ClangArmToolchain.cmake $out/lib/cmake/ClangArmToolchain.cmake
+
+      f=$out/lib/cmake/ClangArmToolchain.cmake
+      if cat $f | grep "set(CMAKE_C_COMPILER"; then
+        echo "CMAKE_C_COMPILER found in $f"
+      else
+        echo "CMAKE_C_COMPILER not found in $f"
+        exit 1
+      fi
+    '';
+  };
+in
+drv
